2522959afee58a9a7b3bb22b454a73c0489870cb
Protor_fenix <Protor_fenix@mail.ru>
2019-07-08 10:27:00 +0300

    ДЗ-3:   -кнопка открыть закрыть корзину   -корзина базовый класс

--- js/main.js
+++ js/main.js
@@ -52,17 +52,42 @@ class ProductItem {
         this.price = product.price;
         this.product_name = product.product_name;
         this.id_product = product.id_product;
-        this.img = img
+        this.img = img;
     }
     render(){
         return `<div class="product-item">
-                 <img src="${this.img}" alt="${this.product_name}">
-                 <div class="desc">
-                     <h3>${this.product_name}</h3>
-                     <p>${this.price}</p>
-                     <button class="buy-btn">Купить</button>
-                 </div>
-             </div>`
+                    <img src="${this.img}" alt="${this.product_name}">
+                    <div class="desc">
+                        <h3>${this.product_name}</h3>
+                        <p>${this.price}</p>
+                        <button class="buy-btn" data-id="${this.id_product}">Купить</button>
+                    </div>
+                </div>`
+    }
+    renderInBasket() {
+        return `<div class="cart-item">
+                    <img src="${this.img}" alt="${this.img}">
+                    <p>"${this.product_name}"</p>
+                    <div class="counter"></div>
+                    <button data-id="${this.id_product}">Удалить</button>
+                </div>`
+    }
+
+}
+
+class BacketItem {
+    constructor(product, img="img/default.png") {
+        this.product_name = product.product_name;
+        this.id_product = product.id_product;
+        this.img = img;
+    }
+    render() {
+        return `<div class="cart-item">
+                    <img src="${this.img}" alt="${this.img}">
+                    <p>"${this.img}"</p>
+                    <div class="counter">1</div>
+                    <button>Удалить</button>
+                </div>`
     }
 }
 
@@ -71,8 +96,23 @@ console.log(products.calcSum());
 
 
 class Cart {
-    constructor(){
-        // this.some здесь будет что-то
+    constructor(container='.cart') {
+        this.container = container;
+        this._init();
+
     }
-    // some(){} - делает что-то
+    _init() {
+        const buyBtns = document.querySelectorAll('.buy-btn');
+        for (const btn of buyBtns) {
+            btn.addEventListener('click', (event) => {
+
+            })
+        }
+    }
+    addProductToBasket() {
+        console.log('111');
+    }
+
 }
+
+const cart = new Cart();
