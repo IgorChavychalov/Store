d7bfa9badf36263324a14f5018229f49cb516fb3
Protor_fenix <Protor_fenix@mail.ru>
2019-07-23 18:22:12 +0300

    HW-7:  -исправил ошибку в "добавления в userCart.json"  -router

--- server/server.js
+++ server/server.js
@@ -1,61 +1,21 @@
 const express = require('express');
 const fs = require('fs');
-const cart = require('./cart');
+const cart = require('./cartRouter');
 const app = express();
 
 app.use(express.json());
 app.use('/', express.static('client'));
+app.use('/api/cart', cart);
+
 
 app.get('/api/products', (req, res) => {
-  fs.readFile('server/db/products.json', 'utf8', (err, data) => {
-    if (err) {
-      res.send({result: 0, text: 'error'});
-    } else {
-      res.send(data);
-    }
-  })
-});
-app.get('/api/cart', (req, res) => {
-  fs.readFile('server/db/userCart.json', 'utf8', (err, data) => {
-    if (err) {
-      res.send({result: 0, text: 'error'});
-    } else {
-      res.send(data);
-    }
-  })
-});
-
-app.post('/api/cart', (req, res) => {
-  fs.readFile('server/db/userCart.json', 'utf8', (err, data) => {
-    if (err) {
-      res.send({result: 0, text: 'error'});
-    } else {
-      let newCart = cart.add(JSON.parse(data), req);
-      fs.writeFile('server/db/userCart.json', newCart, (err) => {
-        if (err) {
-          res.send({result: 0, text: 'error'});
+    fs.readFile('server/db/products.json', 'utf8', (err, data) => {
+        if(err){
+            res.send({result: 0, text: 'error'});
         } else {
-          res.send({result: 1, text: 'ok'});
+            res.send(data)
         }
-      })
-    }
-  })
-});
-app.put('/api/cart/:id', (req, res) => {
-  fs.readFile('server/db/userCart.json', 'utf8', (err, data) => {
-    if (err) {
-      res.send({result: 0, text: 'error'});
-    } else {
-      let newCart = cart.change(JSON.parse(data), req);
-      fs.writeFile('server/db/userCart.json', newCart, (err) => {
-        if (err) {
-          res.send({result: 0, text: 'error'});
-        } else {
-          res.send({result: 1, text: 'ok'});
-        }
-      })
-    }
-  })
+    })
 });
 
 app.listen(3000, () => console.log('Started at port 3000...'));
